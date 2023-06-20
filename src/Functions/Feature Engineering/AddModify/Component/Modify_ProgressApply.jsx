7878c02900095bd433a6b799cf9a409c40e765d9
ah-naf <sheikhahnafshifat@gmail.com>
2023-06-21 00:56:46 +0600

    Progress Apply

--- src/Functions/Feature Engineering/AddModify/Component/Modify_ProgressApply.jsx
+++ src/Functions/Feature Engineering/AddModify/Component/Modify_ProgressApply.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setData } from "../../../../Slices/FeatureEngineeringSlice";
 
 function Modify_ProgressApply({ csvData }) {
+  const [func, setFunction] = useState('Compute All Features using RDKit')
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(setData({
+      'select_function': func
+    }))
+  }, [func, dispatch])
+
   return (
     <div className="mt-8">
       <div className="flex flex-col gap-1">
         <label htmlFor="">Select Function</label>
-        <select className="px-2 py-3 rounded-lg" name="" id="">
+        <select className="px-2 py-3 rounded-lg" name="" id="" onChange={e => setFunction(e.target.value)}>
           <option value="Compute All Features using RDKit">
             Compute All Features using RDKit
           </option>
